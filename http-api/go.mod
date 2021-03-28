94a16ed2baa5b84b8de7049c23221bfab7423313
wuchuheng <wuchuheng@163.com>
2021-03-28 19:44:02 +0800

    添加中间件

--- http-api/go.mod
+++ http-api/go.mod
@@ -2,10 +2,4 @@ module http-api
 
 go 1.15
 
-require (
-	github.com/gorilla/mux v1.8.0
-	golang.org/x/mod v0.4.2 // indirect
-	golang.org/x/net v0.0.0-20210326220855-61e056675ecf // indirect
-	golang.org/x/sys v0.0.0-20210326220804-49726bf1d181 // indirect
-	golang.org/x/tools v0.1.0 // indirect
-)
+require github.com/gorilla/mux v1.8.0
--- http-api/main.go
+++ http-api/main.go
@@ -15,36 +15,33 @@ func main() {
 	router.HandleFunc("/articles", articleStoreHandler).Methods("POST").Name("articles.index")
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 
+	router.Use(forceHTMLMiddleware)
+
 	http.ListenAndServe(":3000", router)
 }
 
 // 首页
 func homeHandle(w http.ResponseWriter, r *http.Request)  {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "文章首页")
 }
 
 // 关于
 func aboutHandler(w http.ResponseWriter, r *http.Request)  {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, "about")
 }
 
 // 文章详情
 func articlesShowHandler(w http.ResponseWriter, r *http.Request)  {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, "article detail" + r.RequestURI)
 }
 
 // 文章列表
 func articlesIndexHandler(w http.ResponseWriter, r *http.Request)  {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, "articles")
 }
 
 // 添加文章
 func articleStoreHandler(w http.ResponseWriter, r *http.Request)  {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, "stored the article" + r.RequestURI)
 }
 
@@ -53,3 +50,12 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request)  {
 	fmt.Fprintf(w, "404")
 }
 
+// 中间件
+func forceHTMLMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 1. 设置标头
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		// 2. 继续处理请求
+		h.ServeHTTP(w, r)
+	})
+}
