ff72f1a2a5f81d4f32a67766f346170771f0adec
wuchuheng <wuchuheng@163.com>
2021-03-28 14:26:00 +0800

    尝试 http.ServerMux

--- http-api/go.mod
+++ http-api/go.mod
@@ -1,3 +1,10 @@
 module http-api
 
 go 1.15
+
+require (
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/net v0.0.0-20210326220855-61e056675ecf // indirect
+	golang.org/x/sys v0.0.0-20210326220804-49726bf1d181 // indirect
+	golang.org/x/tools v0.1.0 // indirect
+)
--- http-api/main.go
+++ http-api/main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/about/", handlerAbountFunc)
 	http.ListenAndServe(":3000", nil)
 }
 
 func handlerFunc(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html; charset-8")
 	fmt.Fprint(w, "<h1>Hello, </h1>")
+}
+
+func handlerAbountFunc(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html; charset-8")
+	id := strings.SplitN(r.URL.Path, "/", 3)[2]
+	fmt.Fprint(w, "id: " + id)
 }
