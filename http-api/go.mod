e5bd86e1e34b0f90b9711818a855dc80315a858b
wuchuheng <wuchuheng@163.com>
2021-03-28 15:17:21 +0800

    集成 Gorilla Mux 路由

--- http-api/go.mod
+++ http-api/go.mod
@@ -3,6 +3,7 @@ module http-api
 go 1.15
 
 require (
+	github.com/gorilla/mux v1.8.0
 	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/net v0.0.0-20210326220855-61e056675ecf // indirect
 	golang.org/x/sys v0.0.0-20210326220804-49726bf1d181 // indirect
--- http-api/main.go
+++ http-api/main.go
@@ -2,23 +2,54 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
-	"strings"
 )
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.HandleFunc("/about/", handlerAbountFunc)
-	http.ListenAndServe(":3000", nil)
+	router := mux.NewRouter()
+	router.HandleFunc("/", homeHandle).Methods("GET").Name("home")
+	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
+	router.HandleFunc("/articles/{id:\\d+}", articlesShowHandler).Methods("GET").Name("article.show")
+	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
+	router.HandleFunc("/articles", articleStoreHandler).Methods("POST").Name("articles.index")
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+	http.ListenAndServe(":3000", router)
 }
 
-func handlerFunc(w http.ResponseWriter, r *http.Request)  {
-	w.Header().Set("Content-Type", "text/html; charset-8")
-	fmt.Fprint(w, "<h1>Hello, </h1>")
+// 首页
+func homeHandle(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "文章首页")
 }
 
-func handlerAbountFunc(w http.ResponseWriter, r *http.Request)  {
-	w.Header().Set("Content-Type", "text/html; charset-8")
-	id := strings.SplitN(r.URL.Path, "/", 3)[2]
-	fmt.Fprint(w, "id: " + id)
+// 关于
+func aboutHandler(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "about")
 }
+
+// 文章详情
+func articlesShowHandler(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "article detail" + r.RequestURI)
+}
+
+// 文章列表
+func articlesIndexHandler(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "articles")
+}
+
+// 添加文章
+func articleStoreHandler(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "stored the article" + r.RequestURI)
+}
+
+// 404 错误
+func notFoundHandler(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintf(w, "404")
+}
+
