c851da475b533e7fa0635c91f533514c3547513c
wuchuheng <wuchuheng@163.com>
2021-03-31 20:15:19 +0800

    重构中间件

--- http-api/main.go
+++ http-api/main.go
@@ -1,70 +1,17 @@
 package main
 
 import (
-	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"http-api/app/http/middlewares"
 	"http-api/bootstrap"
-	"http-api/pkg/database"
-	"http-api/pkg/logger"
 	"net/http"
-	"strings"
 )
 
 var router = mux.NewRouter().StrictSlash(true)
 
-var db *sql.DB
-
-
-func createTables()  {
-	createArticlesSQL := `
-		CREATE TABLE IF NOT EXISTS articles(
-			id bigint(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-			title varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-			body longtext COLLATE utf8mb4_unicode_ci
-		);
-	`
-	_, err := db.Exec(createArticlesSQL)
-	logger.LogError(err)
-}
-
 func main() {
-	database.InitDB()
-	db = database.DB
 	bootstrap.SetupDB()
-
 	router = bootstrap.SetupRoute()
-	createTables()
-	router.Use(forceHTMLMiddleware)
-	http.ListenAndServe(":3000", removeTrailingSlash(router))
-}
-
-func removeTrailingSlash(next http.Handler) http.Handler  {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
-// 文章详情
-type Article struct {
-	Title, Body string
-	ID 	int64
-}
-
-// 中间件
-func forceHTMLMiddleware(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1. 设置标头
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		// 2. 继续处理请求
-		h.ServeHTTP(w, r)
-	})
-}
-
-func getRouteVariable(parameterName string, r *http.Request) string {
-	vars := mux.Vars(r)
-	return vars[parameterName]
+	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
 }
--- http-api/routes/web.go
+++ http-api/routes/web.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gorilla/mux"
 	"http-api/app/http/controllers"
+	"http-api/app/http/middlewares"
 	"net/http"
 )
 
@@ -16,9 +17,10 @@ func RegisterWebRoutes(r *mux.Router)  {
 	r.HandleFunc("/articles/{id:[0-9]+}", ac.Show).Methods("GET").Name("articles.show")
 	r.HandleFunc("/articles", ac.Index).Methods("GET").Name("articles.index")
 	r.HandleFunc("/articles/create", ac.Create).Methods("GET").Name("articles.create")
-
 	r.HandleFunc("/articles", ac.Store).Methods("POST").Name("articles.store")
 	r.HandleFunc("/articles/{id:[0-9]+}/edit", ac.Edit).Methods("GET").Name("articles.edit")
 	r.HandleFunc("/articles/{id:[0-9]+}", ac.Update).Methods("POST").Name("articles.update")
 	r.HandleFunc("/articles/{id:[0-9]+}/delete", ac.Delete).Methods("POST").Name("articles.delete")
+
+	r.Use(middlewares.ForceHTML)
 }
