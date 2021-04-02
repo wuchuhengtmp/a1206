d8265257855c0e569e78198bc639016860398d69
wuchuheng <wuchuheng@163.com>
2021-04-02 22:01:23 +0800

    [http-api]test:优化测试机制

--- http-api/tests/api_test.go
+++ http-api/tests/api_test.go
@@ -1,5 +1,5 @@
 /**
- * @Desc    The tests is part of http-api
+ * @Desc    HTTP api integration testing
  * @Author  wuchuheng<wuchuheng@163.com>
  * @Blog    https://wuchuheng.com
  * @DATE    2021/4/2
@@ -17,42 +17,42 @@ import (
 	"testing"
 )
 
-
-func TestAllApi(t *testing.T)  {
+func TestAllApi(t *testing.T) {
 	baseURL = baseURL + "/api"
-	type postDataType map[string][]string
-
 	var tests = []struct {
-		method 	string
-		url 	string
-		expected int
-		data url.Values
-	} {
-		{"POST", "/authorizations", 200,url.Values{"username": {"admin"}, "password": {"password"}}},
+		method            string
+		url               string
+		expected          int
+		data              url.Values
+		expectedIsSuccess bool
+	}{
+		{"POST", "/authorizations", 200, url.Values{"username": {"admin"}, "password": {"password"}}, true},
+		{"POST", "/authorizations", 200, url.Values{"username": {"admin"}, "password1": {"password"}}, false},
 	}
 	for _, test := range tests {
 		t.Logf("Currently requested link: %v \n", test.url)
 		var (
 			resp *http.Response
-			err error
+			err  error
 		)
 		switch {
 		case test.method == "POST":
-			resp, err = http.PostForm(baseURL + test.url, test.data)
+			resp, err = http.PostForm(baseURL+test.url, test.data)
 		default:
 			resp, err = http.Get(baseURL + test.url)
 		}
-		assert.NoError(t, err, "It was error to request " + test.url)
+		assert.NoError(t, err, "It was error to request "+test.url)
 		body, _ := ioutil.ReadAll(resp.Body)
-		var resData struct{
-			IsSuccess bool 		  `json:"isSuccess"`
+		var resData struct {
+			IsSuccess bool `json:"isSuccess"`
 		}
 		json.Unmarshal(body, &resData)
-		assert.Equal(t, resData.IsSuccess, true,
-			test.url +
-				" method: " + test.method +
-				"  status code " +
-				strconv.Itoa(test.expected) +
-				" must be returned")
+		assert.Equal(t, resData.IsSuccess, test.expectedIsSuccess,
+			test.url+
+				" method: "+test.method+
+				" status code "+
+				strconv.Itoa(test.expected)+
+				" expect: "+strconv.FormatBool(test.expectedIsSuccess)+"\n"+
+				" result: "+strconv.FormatBool(resData.IsSuccess))
 	}
 }
